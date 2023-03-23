40ca14b0b7440f700cfe8a8a66f99af6db40980e
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2023-03-23 07:38:42 +0100

    Update package.d

--- source/models/transportations/package.d
+++ source/models/transportations/package.d
@@ -1,4 +1,9 @@
 module models.transportations;
 
-public import models.transportations.attributes;
-public import models.transportations.entities;
+public {
+  import models.transportations.attributes;
+  import models.transportations.elements;
+  import models.transportations.entities;
+  import models.transportations.usecases;
+  import models.transportations.values;  
+}
